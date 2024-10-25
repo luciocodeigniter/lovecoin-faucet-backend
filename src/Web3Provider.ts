3ae9b319e9d7e745357edf9feeb087775b521c4e
Lucio <luciocodeigniter@gmail.com>
2024-10-24 23:38:30 -0300

    trabalhando na função mintAndTransfer

--- src/Web3Provider.ts
+++ src/Web3Provider.ts
@@ -1,7 +1,33 @@
 /**
  * Arquivo responsável por prover métodos para comunicação com a blockchain
  */
+import Web3 from "web3";
+const ABI = require("./abi.json"); // dá pra fazer com `import`, mas tem que ajustar o tsconfig
 
-export async function mintAndTransfer(to:string) {
-    return "mintAndTransfer";
+const web3Instance = new Web3(
+    // Provider -> aqui dizemos para o Web3 pra onde deve enviar os comandos JSON-RPC
+    // Para realizar as operações na blockchain
+    `${process.env.NODE_URL}`
+);
+
+// Cria a conta a partir da chave privada para se conectar à blockchain
+const account = web3Instance.eth.accounts.privateKeyToAccount("0x" + process.env.PRIVATE_KEY);
+web3Instance.eth.accounts.wallet.add(account);
+
+// agora preciso injetar essa `account` no objeto `web3Instance`
+// isso garante que todas as chamas serão do owner
+web3Instance.eth.accounts.wallet.add(account);
+
+export async function mintAndTransfer(to: string): Promise<string> {
+    const contractInstance = new web3Instance.eth.Contract(ABI, `${process.env.CONTRACT_ADDRESS}`, {
+        from: `${process.env.WALLET}`
+    });
+
+    try {
+        const transaction = await contractInstance.methods.mint(to).send();
+        return transaction.transactionHash;
+    } catch (error) {
+        console.error("Erro ao realizar mint:", error);
+        throw error;
+    }
 }
