aee577357e8ab4281f3529c411d756e4403dbe0c
Lucio <luciocodeigniter@gmail.com>
2024-10-24 21:04:17 -0300

    primeiro commit do backend

---
+++ src/Web3Provider.ts
@@ -0,0 +1,7 @@
+/**
+ * Arquivo responsável por prover métodos para comunicação com a blockchain
+ */
+
+export async function mintAndTransfer(to:string) {
+    return "mintAndTransfer";
+}
---
+++ src/index.ts
@@ -0,0 +1,32 @@
+import dotenv from 'dotenv';
+dotenv.config();
+
+// importamos o arquivo responsável por comunicar-se coma blockchain
+import { mintAndTransfer } from './Web3Provider';
+
+// importamos o `express` e as tipagens
+import express, { Request, Response, NextFunction } from 'express';
+import morgan from 'morgan'; // faz o logging das requisições
+
+const PORT: number = parseInt(`${process.env.PORT || 3001}`);
+
+const app = express();
+
+// `tiny` é o nível de logs
+// toda requisição que chegar para o backend 
+// é processada no morgan primeiramente
+app.use(morgan('tiny'));
+
+app.post("/mint/:wallet", async (req: Request, res: Response, next: NextFunction) => {
+
+    try {
+        const tx = await mintAndTransfer(req.params.wallet);
+        res.json(tx);
+    } catch (error) {
+        res.status(500).json(error);
+    };
+});
+
+app.listen(PORT, () => {
+    console.log(`Server is listening at ${PORT}`);
+});
